255fee8cb517b58b7e7a84c96ab7ca15c8ef06ca
Sunil-Hule <sunilhule4444@gmail.com>
2026-07-08 20:31:48 +0530

    Set site title

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Sunil Hule';
-export const SITE_DESCRIPTION = 'Books and Ideas';
+export const SITE_DESCRIPTION = '';
